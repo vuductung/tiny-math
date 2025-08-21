67e067156eb4a888eff92f0319193cac8a103260
Tung <tungvuduc@outlook.de>
2025-08-21 11:36:06 +0200

    Refactor check_for_square_matrix function to remove unnecessary newline

--- tiny-math/linear-algebra.py
+++ tiny-math/linear-algebra.py
@@ -4,4 +4,3 @@ def check_for_square_matrix(matrix):
     if len(matrix.shape) != 2:
         raise IndexError("Input must be a 2-dimensional array.")
     return matrix.shape[0] == matrix.shape[1]
-
