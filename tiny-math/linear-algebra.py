36577ada0099a8cea20b52155e0300137067c4ab
Tung <tungvuduc@outlook.de>
2025-08-21 11:53:04 +0200

    Remove tiny-math module files including __init__.py and linear algebra utilities

--- tiny-math/linear-algebra.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-def check_for_square_matrix(matrix):
-    if len(matrix.shape) != 2:
-        raise IndexError("Input must be a 2-dimensional array.")
-    return matrix.shape[0] == matrix.shape[1]
--- tiny_math/__init__.py
+++ tiny_math/__init__.py
@@ -1,3 +1,5 @@
 """tinymath - An open-source Python package"""
 
 __version__ = "0.1.0"
+
+
